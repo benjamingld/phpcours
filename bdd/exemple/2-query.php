28be4096d1e1d592d2ba8dbb7d6387db8c606cc0
Benjamin Gillard <gillard.benjamin59@orange.fr>
2022-07-28 17:18:21 +0100

    ok

--- bdd/exemple/2-query.php
+++ bdd/exemple/2-query.php
@@ -4,8 +4,44 @@ include_once "config/setup.php";
 
 $connexion = getPDO();
 
-$sql = "SELECT * FROM utilisateur";
+$sql1 = "SELECT * FROM utilisateur";
 
-$result = $connexion->query($sql);
+$sql2 = "";                                                
+if(isset($_GET['user_id'])){                     //equivalent au htmlspecialchars
+    $sql2 = "SELECT * FROM utilisateur WHERE id=".$connexion->quote($_GET['user_id']);
+}
+
+var_dump($sql1,$sql2);
+
+
+$result = $connexion->query($sql1);
 
 var_dump($result);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border=1 cellpading=3>
+        <?php 
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        ?>   
+            <tr>
+                <td><?=$row['id']?></td>
+                <td><?=$row['nom']?></td>
+                <td><?=$row['prenom']?></td>
+                <td><?=$row['mail']?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    
+    </table>
+</body>
+</html>
--- bdd/exemple/config/define.php
+++ bdd/exemple/config/define.php
@@ -1,4 +1,5 @@
 <?php
+//lancement de la console, entrer le user , le mot de passe et use bibliothque.
 define("DSN", "mysql:host=localhost; dbname=bibliotheque2; port=3306");
 define('DB_USER','root');
 define('DB_PASS','');
